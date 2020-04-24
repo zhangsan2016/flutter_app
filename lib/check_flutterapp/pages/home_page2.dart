b69585544ea5dd5751634202bef3b741f3b27342
liu <602694077@qq.com>
2020-04-24 10:22:27 +0800

    2020年4月24日 10:22:06 添加商品推荐功能

--- lib/check_flutterapp/pages/home_page2.dart
+++ lib/check_flutterapp/pages/home_page2.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutterapp/check_flutterapp/service/service_method.dart';
@@ -29,13 +30,16 @@ class _HomePage2State extends State<HomePage2> {
             gavgatorList.add(companys);
           }
 
-          return SingleChildScrollView(child: Column(
-            children: <Widget>[
-              SwiperDiy(),
-              TopNavigator(navigatorList: gavgatorList),
-              AdBanner()
-            ],
-          ),);  // 添加 SingleChildScrollView 防止越界
+          return SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SwiperDiy(),
+                TopNavigator(navigatorList: gavgatorList),
+                AdBanner(),
+                Recommend()
+              ],
+            ),
+          ); // 添加 SingleChildScrollView 防止越界
           /*   if(snapshot.hasData){
             return Column(
               children: <Widget>[
@@ -74,12 +78,12 @@ class SwiperDiy extends StatelessWidget {
       width: ScreenUtil().setWidth(750),
       child: Swiper(
           itemBuilder: (BuildContext context, int index) {
-            return Image.network("${swiperDateList[index]}", fit: BoxFit.fill);
+            return Image.asset('images/load2.png', fit: BoxFit.fill);
           },
           itemCount: swiperDateList.length,
           pagination: SwiperPagination(), // 导航器（...）
           autoplay: true // 自动播放
-      ),
+          ),
     );
   }
 }
@@ -125,6 +129,9 @@ class TopNavigator extends StatelessWidget {
   }
 }
 
+/**
+ *  广告栏
+ */
 class AdBanner extends StatelessWidget {
   final String adPicture;
 
@@ -132,8 +139,87 @@ class AdBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(child: Image.asset('images/Banner.png'));
+  }
+}
+
+/**
+ *  商品推荐类
+ */
+class Recommend extends StatelessWidget {
+  List recommedList = <String>[
+    'http://forum.xitek.com/pics/202003/98672/9867214/thumb_9867214_1585272562.jpg',
+    'http://forum.xitek.com/pics/202003/2876/287616/thumb_287616_1585233278.jpg',
+    'http://forum.xitek.com/pics/202003/2225/222584/thumb_222584_1585232569.jpg'
+  ];
+
+  Recommend({Key key, this.recommedList}) : super(key: key);
+
+  Widget _titleWidget() {
     return Container(
-        child: Image.asset('images/Banner.png')
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 2.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+              bottom: BorderSide(
+                  width: 0.5, color: Color.fromARGB(0, 0, 255, 255)))),
+      child: Text(
+        '商品推荐',
+        style: TextStyle(color: Colors.pink),
+      ),
+    );
+  }
+
+  // 商品单独项方法
+  Widget _item() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+          height: ScreenUtil().setHeight(330),
+          width: ScreenUtil().setWidth(250),
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                  left: BorderSide(
+                      width: 0.2, color: Colors.grey))),
+        child:Column(
+          children: <Widget>[
+            Image.asset('images/shangping.png'),
+            Text('￥${269}'),
+            Text('￥${385}',style: TextStyle(decoration: TextDecoration.lineThrough,color:Colors.grey),)
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 横向列表方法
+  Widget _recommedList() {
+    return Container(
+        height: ScreenUtil().setHeight(350),
+        margin: EdgeInsets.only(top: 1.0),
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 6,
+          itemBuilder: (context, index) {
+            return _item();
+          },
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(430),
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommedList(),
+        ],
+      ),
     );
   }
 }
