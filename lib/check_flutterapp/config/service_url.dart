106758bffe6f7d383c935f97ec1e91f5eb76c237
liu <602694077@qq.com>
2020-07-27 16:05:52 +0800

    2020年7月27日 16:05:47

--- lib/check_flutterapp/config/service_url.dart
+++ lib/check_flutterapp/config/service_url.dart
@@ -6,6 +6,7 @@ const servicePath={
   'homePageContent':ServiceUrl + 'wxmini/homePageContent', // 商店首页信息
   'homePageBelowConten':ServiceUrl + 'wxmini/homePageBelowConten',// 商城首页热卖商品
   'getMallGoods' : ServiceUrl + 'wxmini/getMallGoods', // 商品分类的商品列表
+  'getGoodDetailById' : ServiceUrl + 'wxmini/getMallGoods', // 商品详细信息
 };
 
 
