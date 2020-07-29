5b1a2d8f788752b8493c45254605a789675e6ed6
liu <602694077@qq.com>
2020-07-29 17:09:20 +0800

    2020年7月29日 17:09:15 商品详情介绍界面，商品评论切换 -- 完成

--- lib/check_flutterapp/pages/details_page/detals_web.dart
+++ lib/check_flutterapp/pages/details_page/detals_web.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutterapp/check_flutterapp/provide/details_Info.dart';
 import 'package:provider/provider.dart';
 
@@ -8,11 +9,30 @@ class DetailsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var goodsDetail = Provider.of<DetailsInfoProvide>(context, listen: false).goodsInfo.data.goodInfo.goodsDetail;
-    return Container(
-      child: Html(
-        data:goodsDetail,
-      ),
-    );
+    var isLeft = Provider.of<DetailsInfoProvide>(context, listen: false).isLeft;
+
+    if(isLeft){
+      return Consumer<DetailsInfoProvide>(builder: (context, counter, _) {
+        return
+          Container(
+            child: Html(
+              data:goodsDetail,
+            ),
+          );
+
+      });
+    }else{
+     return  Container(
+        width: ScreenUtil().setWidth(750),
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child:Text('暂时没有数据'),
+      );
+    }
+
+
+
+
   }
 }
 
