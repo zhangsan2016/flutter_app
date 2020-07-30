cf4b2cd9d9cdd16b44610a6d479ed15a5fdbaa56
liu <602694077@qq.com>
2020-07-30 16:53:01 +0800

    2020年7月30日 16:52:56

--- lib/check_flutterapp/pages/cart_page.dart
+++ lib/check_flutterapp/pages/cart_page.dart
@@ -1,27 +1,68 @@
-
-
-
-import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  List<String> testList = [];
+
   @override
   Widget build(BuildContext context) {
-    print('CartPage');
-    // getHttp();
-    return Scaffold(
-      body: Center(child: Text('购物车界面'),),
+    _show();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            child: ListView.builder(
+                itemCount: testList.length,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text(testList[index]),
+                  );
+                }),
+          ),
+          RaisedButton(
+            onPressed: (){_add();},
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: (){_clear();},
+            child: Text('清空'),
+          ),
+        ],
+      ),
+
     );
   }
 
-  void getHttp() async{
-    try{
-      Response response;
-      response = await Dio().get('http://121.40.194.91:8080/ldsight/deviceAction');
-      print("xxx Response =" + response.toString());
-    }catch(e){
+  void _add() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp = "技术胖是最胖的!";
+    testList.add(temp);
+    prefs.setStringList('testInfo', testList);
+    _show();
+  }
 
-    }
+  void _show() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      if (prefs.getStringList('testInfo') != null) {
+        testList = prefs.getStringList('testInfo');
+      }
+    });
+  }
+
+  void _clear() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.clear(); //全部清空
+    prefs.remove('testInfo'); //删除key键
+    setState((){
+      testList=[];
+    });
   }
 }
