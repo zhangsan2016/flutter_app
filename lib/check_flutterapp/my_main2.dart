9e081e4e8ef1e89fd79f961f4b27d95f7245fb35
liu <602694077@qq.com>
2020-08-13 16:56:57 +0800

    2020年8月13日 16:56:50

--- lib/check_flutterapp/my_main2.dart
+++ lib/check_flutterapp/my_main2.dart
@@ -18,135 +18,99 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: FirstPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  GlobalKey _globalKey = GlobalKey();
-
-  @override
-  void initState() {
-    super.initState();
-
-  //  _requestPermission();
-
-  }
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Save image to gallery"),
+        backgroundColor: Colors.blue,
+        appBar:AppBar(
+          title:Text('FirstPage',style: TextStyle(fontSize: 36.0)),
+          elevation: 0.0,
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              RepaintBoundary(
-                key: _globalKey,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.red,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _saveScreen,
-                  child: Text("Save Local Image"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _getHttp,
-                  child: Text("Save network image"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _saveVideo,
-                  child: Text("Save network video"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 15),
-                child: RaisedButton(
-                  onPressed: _saveGif,
-                  child: Text("Save Gif to gallery"),
-                ),
-                width: 200,
-                height: 44,
-              ),
-            ],
+        body:Center(
+          child: MaterialButton(
+            child: Icon(
+              Icons.navigate_next,
+              color:Colors.white,
+              size:64.0,
+            ),
+            onPressed: (){
+           //   Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context){return SecondPage();}));
+
+              Navigator.of(context).push(CustomRoute(SecondPage()));
+            },
           ),
-        ));
-  }
-
-
-
-  _saveScreen() async {
-    RenderRepaintBoundary boundary =
-    _globalKey.currentContext.findRenderObject();
-    ui.Image image = await boundary.toImage();
-    ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    final result =
-    await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
-    print(result);
-    _toastInfo(result.toString());
-  }
-
-  _getHttp() async {
-    var response = await Dio().get(
-        "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
-        options: Options(responseType: ResponseType.bytes));
-    final result = await ImageGallerySaver.saveImage(
-        Uint8List.fromList(response.data),
-        quality: 60,
-        name: "hello");
-    print(result);
-    _toastInfo("$result");
-  }
-
-  _saveGif() async {
-    var appDocDir = await getTemporaryDirectory();
-    String savePath = appDocDir.path + "/temp.gif";
-    String fileUrl =
-        "https://hyjdoc.oss-cn-beijing.aliyuncs.com/hyj-doc-flutter-demo-run.gif";
-    await Dio().download(fileUrl, savePath);
-    final result = await ImageGallerySaver.saveFile(savePath);
-    print(result);
-    _toastInfo("$result");
-  }
-
-  _saveVideo() async {
-    var appDocDir = await getTemporaryDirectory();
-    String savePath = appDocDir.path + "/temp.mp4";
-    String fileUrl =
-        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
-    await Dio().download(fileUrl, savePath, onReceiveProgress: (count, total) {
-      print((count / total * 100).toStringAsFixed(0) + "%");
-    });
-    final result = await ImageGallerySaver.saveFile(savePath);
-    print(result);
-    _toastInfo("$result");
-  }
-
-  _toastInfo(String info) {
-    showToast(info,position: ToastPosition.bottom);
+        )
+    );
   }
 }
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.pinkAccent,
+        appBar: AppBar(
+          title: Text('SecondPage',style:TextStyle(fontSize:36.0),),
+          backgroundColor: Colors.pinkAccent,
+          leading:Container(),
+          elevation: 0.0,
+        ),
+        body:Center(
+          child: MaterialButton(
+            child: Icon(
+                Icons.navigate_before,
+                color:Colors.white,
+                size:64.0
+            ),
+            onPressed: ()=>Navigator.of(context).pop(),
+          ),
+        )
+    );
+  }
+
+
+
+}
+
+
+class CustomRoute extends PageRouteBuilder{
+  final Widget widget;
+  CustomRoute(this.widget)
+      :super(
+      transitionDuration:const Duration(seconds:5),
+      pageBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2){
+        return widget;
+      },
+      transitionsBuilder:(
+          BuildContext context,
+          Animation<double> animation1,
+          Animation<double> animation2,
+          Widget child){
+        return FadeTransition(
+          opacity: Tween(begin:0.0,end :1.0).animate(CurvedAnimation(
+              parent:animation1,
+              curve:Curves.fastOutSlowIn
+          )),
+          child: child,
+        );
+      }
+
+  );
+
+
+
+
+}
+
+
+
+
